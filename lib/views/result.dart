ddff2b4d9f45884d1e303626e4b9f267385f3d7f
goonerpark <128669715+goonerpark@users.noreply.github.com>
2024-12-18 09:48:03 +0900

    result.dart

--- lib/views/result.dart
+++ lib/views/result.dart
@@ -15,19 +15,18 @@ class ResultPage extends StatelessWidget {
           style: TextStyle(color: Colors.black),
         ),
         backgroundColor: Colors.white,
-        elevation: 1, // 가벼운 그림자 효과
+        elevation: 1,
         centerTitle: true,
         iconTheme: const IconThemeData(color: Colors.black),
       ),
       body: Container(
         width: double.infinity,
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-        color: Colors.white, // 배경 흰색 유지
+        color: Colors.white, 
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            // 결과 텍스트
             Text(
               '1분간 평균 반복 속도: ${(count / 60).toStringAsFixed(2)} 회/s\n총 수행 횟수: $count 회',
               style: const TextStyle(
@@ -38,7 +37,6 @@ class ResultPage extends StatelessWidget {
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 30),
-            // 확인 버튼
             ElevatedButton(
               style: ElevatedButton.styleFrom(
                 padding: EdgeInsets.zero,
@@ -55,7 +53,7 @@ class ResultPage extends StatelessWidget {
               child: Ink(
                 decoration: BoxDecoration(
                   gradient: const LinearGradient(
-                    colors: [Color(0xff6A11CB), Color(0xff2575FC)], // 그라데이션 색상
+                    colors: [Color(0xff6A11CB), Color(0xff2575FC)], 
                     begin: Alignment.topLeft,
                     end: Alignment.bottomRight,
                   ),
